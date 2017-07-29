81152a41edb8ac9f346bbe2e4f8b6d9ae081d0d3
tz1006 <a@omg.tf>
2017-07-29 14:21:49 +0800

    Update vestacp.sh

--- vestacp.sh
+++ vestacp.sh
@@ -8,10 +8,11 @@ EMAIL="v@omg.tf"
 PASSWORD="ztang15"
 
 # 安装vestacp
-curl -O http://vestacp.com/pub/vst-install.sh
-
+# curl -O http://vestacp.com/pub/vst-install.sh
+curl -O http://vestacp.com/pub/vst-install-rhel.sh
 echo "#!/usr/bin/expect
-spawn bash vst-install.sh --nginx yes --phpfpm yes --apache no --named yes --remi yes --vsftpd yes --proftpd no --iptables yes --fail2ban no --quota no --exim yes --dovecot yes --spamassassin yes --clamav no --mysql yes --postgresql no --hostname $HOSTNAME --email $EMAIL --password $PASSWORD 
+# spawn bash vst-install.sh --nginx yes --phpfpm yes --apache no --named yes --remi yes --vsftpd yes --proftpd no --iptables yes --fail2ban no --quota no --exim yes --dovecot yes --spamassassin yes --clamav no --mysql yes --postgresql no --hostname $HOSTNAME --email $EMAIL --password $PASSWORD 
+spawn bash vst-install-rhel.sh --nginx yes --phpfpm yes --apache no --named yes --remi yes --vsftpd yes --proftpd no --iptables yes --fail2ban no --quota no --exim yes --dovecot yes --spamassassin yes --clamav no --mysql yes --postgresql no --hostname $HOSTNAME --email $EMAIL --password $PASSWORD 
 sleep 10
 expect \"continue\"
 send \"y\r\"
